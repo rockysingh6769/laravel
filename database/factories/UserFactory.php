b8ded99a61433ea34799625bf1ea545d8259bdc6
rockysingh6769 <rockysingh6769@gmail.com>
2019-07-30 19:34:38 +0530

    insert multiple rows with fake random values with database seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,11 +17,17 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    // return [
+    //    'name' => $faker->name,
+    //     'email' => $faker->unique()->safeEmail,
+    //     'email_verified_at' => now(),
+    //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+    //     'remember_token' => Str::random(10),
+    // ];
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-    ];
+        'name' => Str::random(10),
+        'country_id' => rand(0, 100),
+        'created_at' => date("Y-m-d H:i:s"),
+        'updated_at' => date("Y-m-d H:i:s"),
+    ];    
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,24 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\user;
+//use App\user;
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      *
      * @return void
-     */
+    */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(testing::class);
+        //  DB::table('users')->insert([
+        //   'name' => Str::random(10),
+        //   'country_id' => rand(3, 12),
+        //   'created_at' => date("Y-m-d H:i:s"),
+        //   'updated_at' => date("Y-m-d H:i:s"),
+        // ]);           
+        // factory(App\User::class,10000)->create();
     }
 }
